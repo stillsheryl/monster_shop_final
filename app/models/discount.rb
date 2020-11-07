a6c69375ef006b2c8eda392f74f08a4a5880a3ed
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-11-07 09:13:56 -0700

    Add more specific validations for user input

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -3,4 +3,8 @@ class Discount < ApplicationRecord
 
   validates_presence_of :percentage,
                         :items_needed
+
+  validates :percentage, inclusion: { in: 1..99, message: 'The percentage must be between 1 and 99' }
+
+  validates_numericality_of :items_needed, greater_than: 0
 end
