be9cf459fcb512551eed19518be41e455ed24eb6
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-11-07 08:55:38 -0700

    Add test for validations

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Discount do
   describe 'Relationships' do
     it {should belong_to :merchant}
   end
+
+  describe 'Validations' do
+    it {should validate_presence_of :percentage}
+    it {should validate_presence_of :items_needed}
+  end
 end
