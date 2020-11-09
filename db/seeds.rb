4d3129ea7b4595ed134319ebd8f460ea05c83a36
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-11-09 14:05:03 -0700

    Create more items and one more merchant user in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,9 @@ brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'D
 # Merchant items
 megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 25 )
 megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 30 )
+megan.items.create!(name: 'Donut', description: "I'm a circle!", price: 5, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 24 )
+megan.items.create!(name: 'Jelly', description: "I go on toast!", price: 7, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 15 )
+megan.items.create!(name: 'Toast', description: "I like jelly!", price: 3, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 10 )
 
 brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 33 )
 
@@ -20,6 +23,8 @@ brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image
 lucy = User.create!(name: 'Lucy Ball', address: '124 Main St.', city: 'Denver', state: 'CO', zip: 80205, email: 'lucy@ball.com', password: 'password', role: 0)
 
 sally = User.create!(name: 'Sally Peach', address: '432 Grove St.', city: 'Denver', state: 'CO', zip: 80205, email: 'sally@peach.com', password: 'password', role: 1, merchant_id: megan.id)
+
+ben = User.create!(name: 'Ben Stone', address: '123 Grove St.', city: 'Denver', state: 'CO', zip: 80205, email: 'ben@stone.com', password: 'password', role: 1, merchant_id: brian.id)
 
 bob = User.create!(name: 'Bob Ross', address: '745 Rose St.', city: 'Denver', state: 'CO', zip: 80205, email: 'bob@ross.com', password: 'password', role: 2)
 
