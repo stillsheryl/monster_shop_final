51c962036f668bcec062d88d8e23cbb640455bea
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-11-09 17:13:30 -0700

    Expand test coverage: item from merchant with discounts but not enough quantity for discount

--- spec/features/user/orders/create_spec.rb
+++ spec/features/user/orders/create_spec.rb
@@ -80,6 +80,7 @@ RSpec.describe 'Create Order' do
       @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 54 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 20 )
+      @donut = @megan.items.create!(name: 'Donut', description: "I'm a circle!", price: 5, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 24 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 19 )
       @user = User.create!(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
@@ -93,6 +94,8 @@ RSpec.describe 'Create Order' do
       visit item_path(@hippo)
       click_button 'Add to Cart'
       visit item_path(@giant)
+      click_button 'Add to Cart'
+      visit item_path(@donut)
       click_button 'Add to Cart'
 
       visit '/cart'
@@ -114,7 +117,7 @@ RSpec.describe 'Create Order' do
       expect(current_path).to eq('/profile/orders')
 
       within "#order-#{order.id}" do
-        expect(page).to have_content("Total: $595.00")
+        expect(page).to have_content("Total: $600.00")
       end
     end
   end
