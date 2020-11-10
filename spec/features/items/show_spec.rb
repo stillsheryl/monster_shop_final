ce6bcc8c50921f4c06f24f4d4797cbef70463886
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-11-10 10:45:37 -0700

    Add test can see default image if none is added when item created

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Item Show Page' do
       @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
-      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, active: true, inventory: 3 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
     end
 
@@ -16,11 +16,18 @@ RSpec.describe 'Item Show Page' do
 
       expect(page).to have_content(@ogre.name)
       expect(page).to have_content(@ogre.description)
+      expect(page).to have_css("img[src*='#{@ogre.image}']")
       expect(page).to have_content("Price: #{number_to_currency(@ogre.price)}")
       expect(page).to have_content("Active")
       expect(page).to have_content("Inventory: #{@ogre.inventory}")
       expect(page).to have_content("Sold by: #{@megan.name}")
       expect(page).to have_link(@megan.name)
+    end
+
+    it "I can see the default image if none is added when item is created" do
+      visit "/items/#{@giant.id}"
+
+      expect(page).to have_css("img[src*='https://images.unsplash.com/photo-1604917018135-18fe420b2ce4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80']")
     end
   end
 end
