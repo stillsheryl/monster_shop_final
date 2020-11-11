688db79453130a1574429968b9f03f769ddcc352
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-11-10 19:02:38 -0700

    Update fulfill to search by all orders since current_user is merch user

--- app/controllers/merchant/orders_controller.rb
+++ app/controllers/merchant/orders_controller.rb
@@ -5,7 +5,7 @@ class Merchant::OrdersController < Merchant::BaseController
   end
 
   def fulfill
-    order = current_user.orders.find(params[:id])
+    order = Order.find(params[:id])
     order_item = order.order_items.find(params[:order_item_id])
     order_item.fulfill if order_item.fulfillable?
     order.is_packaged?
